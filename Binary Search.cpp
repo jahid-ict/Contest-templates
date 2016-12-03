0fe33eaa504f6a1b9ea023b68df68e11fa6facfd
Md. Jahidul Hasan <jahid.ict@gmail.com>
2016-12-03 09:12:55 +0600

    Create Binary Search.cpp

---
+++ Binary Search.cpp
@@ -0,0 +1,13 @@
+#define MAXN 100005
+
+int main()
+{
+    i64 lo=1,hi=10000,mid;
+    while(lo<=hi)
+    {
+        mid=(lo+hi)/2LL;
+        if(...) lo=mid+1;
+        else hi=mid-1;
+    }
+    return 0;
+}
